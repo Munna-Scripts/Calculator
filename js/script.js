b7bfc5d6b762591400a995f4f92600bbc1daf6ba
Munna-Scripts <munnascripts@gmail.com>
2025-05-23 12:50:11 +0600

    Calculator Complete

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@ let Display = document.querySelector('.result')
 
 function DisplayResult(Input){
 
+    Display.innerHTML += Input
 }
 
 function ClearDisplay(){
-
+    Display.innerHTML = ''
 }
 
 function Calculate(){
-    
+    try{
+        Display.innerHTML = eval(Display.innerHTML)
+    }
+    catch(error){
+        Display.innerHTML = 'Error'
+    }
 }
